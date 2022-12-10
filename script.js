5009cf3296f286a9e8d300bc93e2eba981f348da
Yuri3358 <yurialmeida917@gmail.com>
2022-12-10 11:58:07 -0300

    feat: char counter

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const root = React.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 function App() {
     const [text, setText] = React.useState(0)
+    const [maxLength, setMaxLength] = React.useState(120)
+    const charleft = maxLength - (text.length || 0)
 
     return (
-        <div>
+        <div id="content">
             <h1>Contador de caracteres</h1>
-            <textarea onChange={e => setText(e.target.value)}></textarea>
+            <textarea onChange={e => setText(e.target.value)} maxLength={maxLength}></textarea>
+            <input type="number" onChange={e => setMaxLength(e.target.value)}></input>
+            <p>{text.length || 0} caracteres</p>
+            <p>{charleft} caracteres restantes</p>
         </div>
     )
 }
