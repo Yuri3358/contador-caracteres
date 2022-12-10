81fe2523bfe8ff82984ca54811c051ca8698d89f
Yuri3358 <yurialmeida917@gmail.com>
2022-12-10 12:02:46 -0300

    style: unnecessary parenthesis removed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 function App() {
     const [text, setText] = React.useState(0)
     const [maxLength, setMaxLength] = React.useState(120)
-    const charleft = maxLength - (text.length || 0)
+    const charleft = maxLength - text.length || 0
 
     return (
         <section id="content">
